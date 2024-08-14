e3ff7d82b38368c2238c57bdc427cf8528e39763
hocineismail <hocineismail75@gmail.com>
2024-08-14 15:00:37 +0100

    Added socketr updates

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 "use strict";
 
 const { Server } = require("socket.io");
+const answer = require("./api/answer/controllers/answer");
 
 let usersSockets = {};
 
@@ -22,93 +23,143 @@ module.exports = {
    */
   bootstrap({ strapi }) {
     //strapi.server.httpServer is the new update for Strapi V4
-    // let io = new Server(strapi.server.httpServer);
-    // io.on("connection", async function (socket) {
-    //   //Listening for a connection from the frontend
-    //   console.log("user connected");
-    //   const token = socket?.handshake?.query?.token;
-    //   console.log(token);
-    //   // console.log(token);
-    //   // // decrypt the jwt
-    //   if (token) {
-    //     const obj = await strapi.plugins[
-    //       "users-permissions"
-    //     ].services.jwt.verify(token);
-    //     console.log(obj);
-    //     if (obj) {
-    //       const user = await strapi.db
-    //         .query("plugin::users-permissions.user")
-    //         .findOne({
-    //           where: {
-    //             id: obj.id,
-    //           },
-    //         });
-    //       if (user) {
-    //         let sameUser = usersSockets[obj.id] ? usersSockets[obj.id] : [];
-    //         usersSockets = {
-    //           ...usersSockets,
-    //           [obj.id]: [...sameUser, socket.id],
-    //         };
-    //         strapi.io = socket;
-    //         strapi.usersSockets = usersSockets;
-    //       }
-    //       socket.on("seen_conversation", async ({ conversationId }) => {
-    //         console.log("LOGS FROM SOCKET seen_conversation");
-    //         console.log(conversationId);
-    //         // Fetch the existing conversation
-    //         const conversation = await strapi.db
-    //           .query("api::conversation.conversation")
-    //           .findOne({
-    //             where: { id: conversationId },
-    //             select: ["id"],
-    //             populate: {
-    //               users_seen_message: true,
-    //             },
-    //           });
-    //         if (conversation) {
-    //           const oldUsersSeenMessage = conversation.users_seen_message;
-    //           // Check if the user is already in the relation
-    //           const userExists = oldUsersSeenMessage.some(
-    //             (item) => item.id === user.id
-    //           );
-    //           if (!userExists) {
-    //             // If not, add the user to the relation
-    //             const newUsersSeenMessage = [
-    //               ...oldUsersSeenMessage.map((item) => item.id),
-    //               user.id,
-    //             ];
-    //             // Update the conversation with the new relation
-    //             await strapi.db.query("api::conversation.conversation").update({
-    //               where: { id: conversationId },
-    //               data: {
-    //                 users_seen_message: newUsersSeenMessage,
-    //               },
-    //             });
-    //           }
-    //         }
-    //       });
-    //       socket.on("disconnect", () => {
-    //         if (user) {
-    //           console.log("user disconnected");
-    //           if (user) {
-    //             if (usersSockets[obj.id]) {
-    //               usersSockets[obj.id] = usersSockets[obj.id].filter(
-    //                 (item) => item !== socket.id
-    //               );
-    //               if (usersSockets[obj.id].length === 0) {
-    //                 // If no more sockets for this user, delete the user entry
-    //                 delete usersSockets[obj.id];
-    //               }
-    //             }
-    //             strapi.io = io;
-    //             strapi.usersSockets = usersSockets;
-    //           }
-    //         }
-    //       });
-    //       strapi.io = io;
-    //       strapi.usersSockets = usersSockets;
-    //     }
-    //   }
-    // });
+    let io = new Server(strapi.server.httpServer);
+    let currentUser = {};
+    io.on("connection", async function (socket) {
+      //Listening for a connection from the frontend
+      console.log("user connected");
+      const token = socket?.handshake?.query?.token;
+      console.log(token);
+      // console.log(token);
+      // // decrypt the jwt
+      if (token) {
+        const obj = await strapi.plugins[
+          "users-permissions"
+        ].services.jwt.verify(token);
+
+        if (obj) {
+          const user = await strapi.db
+            .query("plugin::users-permissions.user")
+            .findOne({
+              where: {
+                id: obj.id,
+              },
+            });
+          if (user) {
+            currentUser = user;
+            let sameUser = usersSockets[obj.id] ? usersSockets[obj.id] : [];
+            usersSockets = {
+              ...usersSockets,
+              [obj.id]: [...sameUser, socket.id],
+            };
+            strapi.io = socket;
+            strapi.usersSockets = usersSockets;
+          }
+          socket.on("seen_conversation", async ({ conversationId }) => {
+            // Fetch the existing conversation
+            const conversation = await strapi.db
+              .query("api::conversation.conversation")
+              .findOne({
+                where: { id: conversationId },
+                select: ["id"],
+                populate: {
+                  users_seen_message: true,
+                },
+              });
+            if (conversation) {
+              const oldUsersSeenMessage = conversation.users_seen_message;
+              // Check if the user is already in the relation
+              const userExists = oldUsersSeenMessage.some(
+                (item) => item.id === user.id
+              );
+              if (!userExists) {
+                // If not, add the user to the relation
+                const newUsersSeenMessage = [
+                  ...oldUsersSeenMessage.map((item) => item.id),
+                  user.id,
+                ];
+                // Update the conversation with the new relation
+                await strapi.db.query("api::conversation.conversation").update({
+                  where: { id: conversationId },
+                  data: {
+                    users_seen_message: newUsersSeenMessage,
+                  },
+                });
+              }
+            }
+          });
+
+          socket.on(
+            "update-quiz",
+            async ({ assignationId, answerId, questionId }) => {
+              const assignation = await strapi.db
+                .query("api::assignation.assignation")
+                .findOne({
+                  where: {
+                    id: assignationId,
+                    etudent: currentUser,
+                  },
+                  select: ["id"],
+                });
+              if (assignation && !assignation.score) {
+                const answer = await strapi.db
+                  .query("api::conversation.conversation")
+                  .findOne({
+                    where: {
+                      question: questionId,
+                      assignation: assignationId,
+                    },
+                    select: ["id"],
+                  });
+                if (answer) {
+                  await strapi.db
+                    .query("api::answer-history.answer-history")
+                    .update({
+                      where: {
+                        question: questionId,
+                        assignation: assignationId,
+                      },
+                      data: {
+                        answer: answerId,
+                      },
+                    });
+                } else {
+                  await strapi.db
+                    .query("api::answer-history.answer-history")
+                    .create({
+                      data: {
+                        question: questionId,
+                        assignation: assignationId,
+                        answer: answerId,
+                      },
+                    });
+                }
+              }
+            }
+          );
+
+          socket.on("disconnect", () => {
+            if (user) {
+              console.log("user disconnected");
+              if (user) {
+                if (usersSockets[obj.id]) {
+                  usersSockets[obj.id] = usersSockets[obj.id].filter(
+                    (item) => item !== socket.id
+                  );
+                  if (usersSockets[obj.id].length === 0) {
+                    // If no more sockets for this user, delete the user entry
+                    delete usersSockets[obj.id];
+                  }
+                }
+                strapi.io = io;
+                strapi.usersSockets = usersSockets;
+              }
+            }
+          });
+          strapi.io = io;
+          strapi.usersSockets = usersSockets;
+        }
+      }
+    });
   },
 };
